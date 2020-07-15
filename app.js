f5e9b92c2797378cc6c5789ede1743e1897cd44b
Jonatas Vieira <jsoovieira@gmail.com>
2020-07-15 17:10:30 -0300

    First commit.

---
+++ app.js
@@ -0,0 +1,15 @@
+//var express = require('express');
+
+//var app = express();
+
+//const mapa = [ {a: "a", b: "b", c: "c"}]
+
+//app.get('/', (req, res) => { res.json(mapa)});
+
+//app.listen(8080, function(){
+    //console.log('Listening on port 8080');
+//});
+
+//module.exports = app;
+
+console.log("Hello world!")
