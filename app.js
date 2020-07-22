a6e3d141d300659f3687d75ab7c9b074afffd315
Jonatas Vieira <jsoovieira@gmail.com>
2020-07-22 14:51:20 -0300

    Creates start script.

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ var app = express();
 
 const mapa = [ {a: "a", b: "b", c: "c"}]
 
-app.get('/', (req, res) => { res.send("Hello Openshift world.")});
+app.get('/', (req, res) => { res.send("Hello Openshift.")});
 
 app.listen(8080, function(){
     console.log('Listening on port 8080');
 });
 
 module.exports = app;
-
-console.log("Hello world!")
